0a77e012a0763381fdf94e0e8327298d01292c17
Blake Stephenson <blake.stephenson@ymail.com>
2024-01-08 16:53:04 -0500

    Pushing edited PyBank script 1/8

---
+++ PyPoll/Analysis/Election_Results_script.py
@@ -0,0 +1,33 @@
+import os
+import csv
+import pandas as pd
+
+election_datacsv = os.path.join("..", "Resources","election_data.csv").replace('\\','/')
+
+print(f"current working dir: {os.getcwd()}")
+print(f"csv path: {election_datacsv}")
+with open(election_datacsv) as election_sheet:
+    
+    electiondata_reader = csv.reader(election_sheet)
+    heading = next(electiondata_reader)
+
+election_df = pd.read_csv(election_datacsv)
+
+
+print("Election Results")
+print("-------------------------------")
+
+# The total number of votes cast
+
+total_votes = election_df["Ballot ID"].nunique()
+print(f"Total Votes: {total_votes}")
+print("--------------------------------")
+
+# A complete list of candidates who recieved votes
+# The percentage of votes each candidate won
+# The total number of votes each candidate won
+
+print(election_df("Candidate")["Ballot ID"].nunique())
+# For Charles Casper Stockham, Diana DeGette and Raymon Anthony Doane
+
+# The winner of the election based on popular vote
